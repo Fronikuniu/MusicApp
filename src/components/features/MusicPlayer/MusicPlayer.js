bb16034ba9b8c1c06097eec1547ad761739b5515
Fronikuniu <Fronikuniu@gmail.com>
2021-07-18 12:53:30 +0200

    Task 3. My solution to the problem with MusicPLayer who playing only one song

--- src/components/features/MusicPlayer/MusicPlayer.js
+++ src/components/features/MusicPlayer/MusicPlayer.js
@@ -2,19 +2,23 @@ import ReactAudioPlayer from 'react-audio-player';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
-const MusicPlayer = ({ title, author, filename, className = '', startAt = 20 }) => {
+const MusicPlayer = ({ title, author, filename, className = '', startAt = 0 }) => {
   const handlePlayer = (e) => {
     console.log(e);
     if (e && e.audioEl) {
       console.log(e.audioEl.current);
       const player = e.audioEl.current;
       player.currentTime = startAt;
+      console.log(player.currentTime);
       player.play();
     }
   };
 
   return (
     <div className={clsx(className)}>
+      <h1 className="text-2xl font-semibold text-blue-400 mb-5">
+        '{title}' <span className="text-xl font-semibold">by {author}</span>
+      </h1>
       <ReactAudioPlayer className="focus:outline-none" ref={handlePlayer} src={process.env.PUBLIC_URL + '/songs/' + filename} autoPlay controls />
     </div>
   );
--- src/components/pages/Song/Song.js
+++ src/components/pages/Song/Song.js
@@ -1,11 +1,30 @@
-import PageTitle from '../../layout/PageTitle/PageTitle'
-import MusicPlayer from '../../features/MusicPlayer/MusicPlayer'
+import PageTitle from '../../layout/PageTitle/PageTitle';
+import MusicPlayer from '../../features/MusicPlayer/MusicPlayer';
+import { connect } from 'react-redux';
+import { getAllSongs } from '../../../redux/subReducers/songsSubReducer';
+import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Song = () => (
+const Song = ({ songs }) => {
+  let { songId } = useParams();
+  songId = parseInt(songId);
+
+  const result = songs.find(({ id }) => id === songId);
+
+  return (
     <section className="page">
-        <PageTitle>Song</PageTitle>
-        <MusicPlayer className="mt-6" title="At The Top" author="David Renda" filename="At_The_Top_David_Renda.mp3" />
+      <PageTitle>Song:</PageTitle>
+      <MusicPlayer className="mt-6" title={result.title} author={result.author} filename={result.filename} />
     </section>
-)
+  );
+};
 
-export default Song
+Song.propTypes = {
+  songs: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  songs: getAllSongs(state),
+});
+
+export default connect(mapStateToProps)(Song);
